d66c70ccb8142217e8392c932cdf3850d2622385
Quinn Michaels <quinn@indra.ai>
2025-02-14 11:29:46 -0800

    update networks to new deva core.

--- feature/methods.js
+++ feature/methods.js
@@ -1,34 +1,49 @@
 export default {
   /**************
-  method: service
+  method: networks
   params: packet
-  describe: The global service feature that installs with every agent
+  describe: The global networks feature that installs with every agent
   ***************/
-  systems(packet) {
+  networks(packet) {
     this.context('feature');
-    const systems = this.systems();
-    const data = {};
     return new Promise((resolve, reject) => {
-      this.question(`#docs raw feature/systems`).then(doc => {
-        data.doc = doc.a.data;
-        const info = [
-          `## Systems`,
-          `::begin:systems:${systems.id}`,
-          `client: ${systems.client_name}`,
-          `concerns: ${systems.concerns.join(', ')}`,
-          `::end:systems:${this.hash(systems)}`,
-        ].join('\n');
-        const text = doc.a.text.replace(/::info::/g, info)
-        return this.question(`#feecting parse ${text}`)
-      }).then(feecting => {
+      const networks = this.networks();
+      const agent = this.agent();
+      const global = [];
+      networks.global.forEach((item,index) => {
+        global.push(`::begin:global:${item.key}:${item.id}`);
+        for (let x in item) {
+          global.push(`${x}: ${item[x]}`);
+        }
+        global.push(`::end:global:${item.key}:${this.lib.hash(item)}`);
+      });
+      const concerns = [];
+      networks.concerns.forEach((item, index) => {
+        concerns.push(`${index + 1}. ${item}`);
+      });
+    
+      const info = [
+        '::BEGIN:NETWORKS',
+        '### Client',
+        `::begin:client:${networks.client_id}`,
+        `id: ${networks.client_id}`,
+        `client: ${networks.client_name}`,
+        '**concerns**',
+        concerns.join('\n'),
+        `::end:client:${this.lib.hash(networks)}`,
+        '### Global',
+        global.join('\n'),
+        '::END:NETWORKS',
+      ].join('\n');
+      this.question(`${this.askChr}feecting parse ${info}`).then(feecting => {
         return resolve({
           text: feecting.a.text,
           html: feecting.a.html,
-          data: systems
+          data: networks.concerns,
         });
       }).catch(err => {
         return this.error(err, packet, reject);
-      })
+      });
     });
   },
 };
